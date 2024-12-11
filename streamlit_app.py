26f34fe26a099c2712ec1244c247294cd245ed98
Yeray Mata Rodriguez <yeraymatarodriguez@gmail.com>
2024-12-11 12:17:49 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,3 @@
 import streamlit as st
 
-st.title("Suma de dos números")
-
-# Pedir el primer número
-num1 = st.number_input("Introduce el primer número", value=0.0)
-
-# Pedir el segundo número
-num2 = st.number_input("Introduce el segundo número", value=0.0)
-
-# Calcular la suma
-resultado = num1 + num2
-
-# Mostrar el resultado
-st.write("La suma de los dos números es:", resultado)
+st.title("Clasificador de mascotas")
